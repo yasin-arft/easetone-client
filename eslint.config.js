18b1aa6dac5d5115ec7d2b784070961bc10cc3a2
yasin-arft <yasinarftsocial@gmail.com>
2024-08-15 00:45:01 +0600

    shadcn setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,5 @@
 import js from '@eslint/js'
-import globals from 'globals'
+import globals, { node } from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
@@ -10,6 +10,7 @@ export default [
     ignores: ['dist'],
     languageOptions: {
       ecmaVersion: 2020,
+      node: true,
       globals: globals.browser,
       parserOptions: {
         ecmaVersion: 'latest',
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { Button } from './components/ui/button'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <Button>Click me</Button>
+    </div>
   </StrictMode>,
 )
