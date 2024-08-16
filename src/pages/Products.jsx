b6e5d2a917215b100f32890cfa8c4c85c0585411
yasin-arft <yasinarftdeveloper@gmail.com>
2024-08-16 14:55:45 +0800

    basic product card designed

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,8 +1,18 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { format } from 'date-fns';
+import { Button } from "@/components/ui/button";
 
 const Products = () => {
-  const { data } = useQuery({
+  const { data: products = [] } = useQuery({
     queryKey: ['easetone', 'products'],
     queryFn: async () => {
       const res = await axios.get('http://localhost:5000/products');
@@ -10,13 +20,38 @@ const Products = () => {
       return res.data
     }
   });
-
-  console.log(data);
-
+  console.log(products);
 
   return (
     <section>
       <h2 className="text-2xl font-bold text-center my-3">Our products</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
+        {
+          products.map(product => {
+            const { _id, name, image, description, brand, category, price, ratings, creationDate, } = product;
+
+            return (
+              <Card key={_id}>
+                <CardHeader>
+                  <CardTitle className="text-2xl">{name}</CardTitle>
+                  <CardDescription>{description}</CardDescription>
+                </CardHeader>
+                <CardContent className="space-y-2">
+                  <p>{category}</p>
+                  <p>Price: {price}</p>
+                  <p>Ratings: {ratings}</p>
+                  <p>
+                    {creationDate && format(new Date(creationDate), 'dd/MM/yyyy, h:mm:ss aaa')}
+                  </p>
+                </CardContent>
+                <CardFooter className="block">
+                  <Button>View Product</Button>
+                </CardFooter>
+              </Card>
+            )
+          })
+        }
+      </div>
     </section>
   );
 };
