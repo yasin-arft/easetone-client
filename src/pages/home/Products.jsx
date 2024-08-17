93bba7ea28aa693dbe1d0243fab64c0a3cd07515
yasin-arft <yasinarftdeveloper@gmail.com>
2024-08-17 17:31:53 +0800

    firebae config setup

--- src/pages/home/Products.jsx
+++ src/pages/home/Products.jsx
@@ -24,8 +24,6 @@ const Products = () => {
   const [maxPrice, setMaxPrice] = useState('');
   const [sort, setSort] = useState('');
   const axiosPublic = useAxiosPublic();
-
-  console.log({sort});
   
   const { data: products = [] } = useQuery({
     queryKey: ['easetone', 'products', currentPage, searchedProducts, brand, category, minPrice, maxPrice, sort],
