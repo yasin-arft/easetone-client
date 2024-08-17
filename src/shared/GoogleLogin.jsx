6f75a3d1a4ad9e9f6a035894c8e3d240b9358cfe
yasin-arft <yasinarftdeveloper@gmail.com>
2024-08-18 00:09:54 +0800

    google login functionality added

--- src/shared/GoogleLogin.jsx
+++ src/shared/GoogleLogin.jsx
@@ -1,9 +1,30 @@
 import { Button } from "@/components/ui/button";
+import { AuthContext } from "@/providers/AuthProvider";
+import { useContext } from "react";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 const GoogleLogin = () => {
+  const { googleSignIn, setLoading } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  // google login handler
+  const handleGoogleLogin = () => {
+    googleSignIn()
+      .then(() => {
+        toast.success('Successfully Logged in');
+        navigate('/profile');
+        setLoading(false);
+      })
+      .catch(() => {
+        toast.error('Unexpected error happened');
+        setLoading(false);
+      })
+  }
+
   return (
     <div className="text-center">
-      <Button>
+      <Button onClick={handleGoogleLogin}>
         Login with google
       </Button>
     </div>
